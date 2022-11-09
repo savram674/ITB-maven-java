2592a775af4bf9a8be3db1d8e8a165c1777e2b96
savram674 <savitharamesh674@gmail.com>
2022-11-09 12:12:12 +0530

    DemoIT B committed

---
+++ src/main/java/in/kmit/DemoProjectITB/App.java
@@ -0,0 +1,16 @@
+package in.kmit.DemoProjectITB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Welcome to IT B" );
+        
+    }
+}
